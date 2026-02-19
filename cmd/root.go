77ce618b03e57a1132d4035f9db87b5039d00343
DKeshavarz <dankeshavarz1075@gmail.com>
2026-02-20 03:10:29 +0330

    chore: add cobra

---
+++ cmd/root.go
@@ -0,0 +1,36 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "gort",
+	Short: "Gort - Fast network port scanner and OS fingerprinting tool",
+	Long: `Gort (Go + Port) is a high-performance network reconnaissance tool that scans
+for open ports, identifies running services, and fingerprints operating systems.
+
+Features:
+  • Nothing
+
+For more information, use 'gort [command] --help'`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("meow")
+	},
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+
+}
+
+
--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
-import "fmt"
+import "github.com/DKeshavarz/gort/cmd"
 
 func main() {
-	fmt.Println("hello")
+	cmd.Execute()
 }
